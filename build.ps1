d017b81e884c5414bb7f6c88f4640d08fa373de2
NKuzmin <nkuzmin@korusconsulting.ru>
2025-01-16 21:34:43 +0300

    Корректировка скрипта сборки

--- build.ps1
+++ build.ps1
@@ -6,6 +6,9 @@ foreach ($target in $targets) {
     $env:GOOS = $target.GOOS
     $env:GOARCH = $target.GOARCH
     $output = "HeartBeatMonitor-$env:GOOS-$env:GOARCH"
-    go build -o $output
+    go build -o ./Release/$env:GOOS/$output
     Write-Host "Built: $output"
+    Copy-Item -Recurse -Force ./templates ./Release/$env:GOOS/
+    Compress-Archive -Path ./Release/$env:GOOS\* -CompressionLevel Fastest -DestinationPath .\Release\$output.zip
+    Remove-Item -Force -Recurse ./Release/$env:GOOS/
 }
