b6c3f1d340c57f075ee15e0003810d7689f478f9
NKuzmin <nkuzmin@korusconsulting.ru>
2025-01-23 21:42:29 +0300

    Добавление обработки маркера warning для плагина, анализирующего свободное место на диске

--- internal/plugins/HardDriveFreeSpacePlugin.go
+++ internal/plugins/HardDriveFreeSpacePlugin.go
@@ -7,8 +7,10 @@ import (
 	"github.com/shirou/gopsutil/v3/disk"
 )
 
-type HardDriveFreeSpacePlugin struct{
-    DriveMountPoint string
+type HardDriveFreeSpacePlugin struct {
+	DriveMountPoint       string
+	WarningValueInGb      float64
+	WarningValueInPercent float64
 }
 
 func (v HardDriveFreeSpacePlugin) Name() string {
@@ -23,9 +25,9 @@ func (plgn HardDriveFreeSpacePlugin) Collect() (map[string]interface{}, error) {
 
 	result := make(map[string]interface{})
 	for _, partition := range partitions {
-        if(plgn.DriveMountPoint != partition.Mountpoint) {
-            continue
-        }
+		if plgn.DriveMountPoint != partition.Mountpoint {
+			continue
+		}
 		usage, err := disk.Usage(partition.Mountpoint)
 		if err != nil {
 			return nil, fmt.Errorf("failed to get usage for partition %s: %v", partition.Mountpoint, err)
@@ -38,8 +40,17 @@ func (plgn HardDriveFreeSpacePlugin) Collect() (map[string]interface{}, error) {
 			"usedPercent": (math.Round(usage.UsedPercent) * 100) / 100,                   // Процент использования
 		}
 
+		partitionTotals["isWarning"] = (partitionTotals["free"].(float64) < plgn.WarningValueInGb) || (partitionTotals["usedPercent"].(float64) < plgn.WarningValueInPercent)
 		result[partition.Mountpoint] = partitionTotals
 	}
 
+	isWarning := false
+
+	for _, value := range result {
+
+		isWarning = isWarning || value.(map[string]interface{})["isWarning"].(bool)
+	}
+
+	result["isWarning"] = isWarning
 	return result, nil
 }
