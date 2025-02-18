377f0153e727e3e6d5cd8f27d3918fc290774d49
NKuzmin <nkuzmin@korusconsulting.ru>
2025-02-18 20:22:57 +0300

    Рефакторинг PluginFactory

--- internal/pluginFactory/PluginFactory.go
+++ internal/pluginFactory/PluginFactory.go
@@ -9,31 +9,36 @@ import (
 
 // Registers all available plugins
 func init() {
-	RegisterPluginType("VersionPlugin", func() plugins.Plugin { return &plugins.VersionPlugin{} })
-	RegisterPluginType("HardDriveFreeSpacePlugin", func() plugins.Plugin { return &plugins.HardDriveFreeSpacePlugin{} })
-	RegisterPluginType("PostgreSqlQueryPlugin", func() plugins.Plugin { return &plugins.PostgreSqlQueryPlugin{} })
-	RegisterPluginType("FolderSizePlugin", func() plugins.Plugin { return &plugins.FolderSizePlugin{} })
-	RegisterPluginType("ServerTimePlugin", func() plugins.Plugin { return &plugins.ServerTimePlugin{} })
+	registeredPlugins["VersionPlugin"] = func() plugins.Plugin { return &plugins.VersionPlugin{} }
+	registeredPlugins["HardDriveFreeSpacePlugin"] = func() plugins.Plugin { return &plugins.HardDriveFreeSpacePlugin{} }
+	registeredPlugins["PostgreSqlQueryPlugin"] = func() plugins.Plugin { return &plugins.PostgreSqlQueryPlugin{} }
+	registeredPlugins["FolderSizePlugin"] = func() plugins.Plugin { return &plugins.FolderSizePlugin{} }
+	registeredPlugins["ServerTimePlugin"] = func() plugins.Plugin { return &plugins.ServerTimePlugin{} }
 }
 
 // Collection of registered plugins
+// Key: plugin name, Value: function that creates a new plugin instance.
 var registeredPlugins = make(map[string]func() plugins.Plugin)
 
-// TODO rename function
-func RegisterPluginType(name string, constructor func() plugins.Plugin) {
-	registeredPlugins[name] = constructor
-}
-
+// Creates a new plugin instance by its name and fills its fields with values from recieved parameters.
+//
+// name - the registered type name of the plugin.
+// params - a map of parameters, where keys match the field names of the plugin's struct.
+//
+// Returns:
+// - A plugin instance (`plugins.Plugin`) if created successfully.
+// - An error (`error`) if:
+//   - The plugin name is not registered.
+//   - A field specified in `params` does not exist in the plugin struct.
+//   - The type of a value in `params` does not match the type of the corresponding struct field.
 func CreatePlugin(name string, params map[string]interface{}) (plugins.Plugin, error) {
 	constructor, exists := registeredPlugins[name]
 	if !exists {
 		return nil, fmt.Errorf("plugin type '%s' not registered", name)
 	}
 
-	// Создаём новый экземпляр плагина через конструктор
 	plugin := constructor()
 
-	// Заполняем поля структуры из params
 	pluginValue := reflect.ValueOf(plugin).Elem()
 	for key, value := range params {
 		field := pluginValue.FieldByName(key)
