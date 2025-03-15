8e256ab78d37dd29a7338b605d25088feb643729
Nikolai Kuzmin <nickkuzmin1301@gmail.com>
2025-03-15 23:54:26 +0300

    Добавлен комментарий к интерфейсу

--- internal/plugins/plugin.go
+++ internal/plugins/plugin.go
@@ -1,5 +1,8 @@
 package plugins
 
+// Common interface for plugin
+// Defines the name of the plugin and
+// and its function for retrieving data
 type Plugin interface {
 	Name() string
 	Collect() (map[string]interface{}, error)
