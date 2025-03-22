81b824a796418171d1550e531683fd88fe89b5a7
Nikolai Kuzmin <nickkuzmin1301@gmail.com>
2025-03-22 11:09:18 +0300

    Рефакторинг плагина работы с докером

--- internal/plugins/DockerContainerStatusPlugin.go
+++ internal/plugins/DockerContainerStatusPlugin.go
@@ -2,6 +2,7 @@ package plugins
 
 import (
 	"context"
+	"reflect"
 	"time"
 
 	"github.com/docker/docker/api/types/container"
@@ -10,43 +11,49 @@ import (
 
 type DockerContainerStatusPlugin struct{}
 
-func (d DockerContainerStatusPlugin) Name() string {
+func (plugin DockerContainerStatusPlugin) Name() string {
 	return "Docker Container Status Plugin"
 }
 
-func (d DockerContainerStatusPlugin) Collect() (map[string]interface{}, error) {
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		return nil, err
-	}
+func (plugin DockerContainerStatusPlugin) Collect() (map[string]interface{}, error) {
 
-	containers, err := cli.ContainerList(context.Background(), container.ListOptions{All: true})
-	if err != nil {
-		return nil, err
-	}
+	results := make(map[string]interface{})
+	results["Type"] = reflect.TypeOf(plugin).Name()
 
-	results := make([]map[string]interface{}, 0)
 	now := time.Now()
+
+	containers, err := GetContainers()
+	if err != nil {
+		return nil, err
+	}
 
 	for _, container := range containers {
 		name := ""
-		state := ""
-
 		if len(container.Names) > 0 {
 			name = container.Names[0]
-			state = container.State
 		}
 
-		uptime := now.Sub(time.Unix(container.Created, 0)).String()
-
-		results = append(results, map[string]interface{}{
-			"name":   name,
-			"state":  state,
-			"uptime": uptime,
-		})
+		results[name] = map[string]interface{}{
+			"state":  container.State,
+			"uptime": now.Sub(time.Unix(container.Created, 0)).Round(time.Second).String(),
+		}
 	}
 
 	return map[string]interface{}{
 		"containers": results,
 	}, nil
+}
+
+func GetContainers() ([]container.Summary, error) {
+	client, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
+
+	containers, err := client.ContainerList(context.Background(), container.ListOptions{All: true})
+	if err != nil {
+		return nil, err
+	}
+
+	return containers, nil
 }
