3745496fd3df883903d7dc405ab93b7515d5fbb6
Nikolai Kuzmin <nickkuzmin1301@gmail.com>
2025-04-24 19:56:39 +0300

    TS код разбит по  модулям

---
+++ src/mutationObserver.ts
@@ -0,0 +1,43 @@
+import {hideNonWarningWidgets} from './render'
+
+/** 
+ * Find divs with 'warning' class 
+ * and marking their parent elements with warning
+ **/
+function processWarnings(): void {
+    document.querySelectorAll("div.warning").forEach((el) => {
+        el.parentElement?.classList.add("parent-warning");
+    });
+
+    hideNonWarningWidgets();
+}
+/**
+ * Create a MutationObserver that watches for changes in the DOM
+ **/
+const observer = new MutationObserver((mutationsList: MutationRecord[]) => {
+    mutationsList.forEach((mutation: MutationRecord) => {
+        if (mutation.type === "childList") {
+            processWarnings();
+        }
+    });
+});
+
+/**
+ * Select the document body as the target to observe for changes
+ */
+const targetNode: Node = document.body;
+
+/**
+ * Define the observer settings to watch 
+ * for changes in child elements and all of its descendants
+ */
+const config: MutationObserverInit = {
+    childList: true,
+    subtree: true
+};
+
+/** 
+ * Start observing chancges in DOM
+ **/
+observer.observe(targetNode, config);
+
---
+++ src/render.ts
@@ -0,0 +1,191 @@
+import * as Utils from './utils';
+import {makeNewWidget} from './gridStackHandler'
+
+
+/**
+ * Render simple widget data, like text, number, etc
+ */
+function renderPrimitive(data: any): string {
+    return `<div class="widget-data">${data}</div>`;
+}
+
+/**
+ Render set of widget indicators
+ */
+function renderArray(data: any[]): string {
+    const itemsHtml = data
+        .map((item) => `<div class="data_array_element">${renderWidgetData(item, 99)}</div>`)
+        .join("");
+    return `<div class="data_array">${itemsHtml}</div>`;
+}
+
+/**
+ * Render agent's plugins data
+ */
+function renderAgentSection(data: any): { html: string; data: any } {
+    if ("agent_name" in data) {
+        const agentName: string = data.agent_name;
+        const duration: number = data.duration;
+
+        const innerData = data.data;
+        const html = `<div class="agent-name">
+                      ${agentName}
+                      <div class="timer agent-duration">Loading time ${duration} seconds</div>
+                    </div>
+                    <div class="agent-data">`;
+        return { html, data: innerData };
+    }
+    return { html: "", data };
+}
+
+/**
+ * Build html for agent title
+ */
+function renderAgentTitleForWidget(data: any): { html: string; data: any } {
+    if ("agent_name" in data) {
+        const agentName: string = data.agent_name;
+        const duration: number = data.duration;
+
+        const innerData = data.data;
+        const html = `<div class="built-in-agent-name">
+                      ${agentName}
+                    </div>
+                    {0}
+                    `;
+        return { html, data: innerData };
+    }
+    return { html: "{0}", data };
+}
+
+/**
+ * Build html for plugin header
+ */
+function renderPluginHeader(pluginName: string): string {
+        const html = `<div class="plugin-name">${pluginName}</div>{0}`;
+        return html;
+
+}
+
+/**
+ * Build html for plugin data
+ */
+function renderPluginData(
+    template: string,
+    data: any,
+    levelClass: number,
+    isWarning: boolean
+): string {
+
+    
+    let html = "";
+    for (const key in data) {
+        if (!data.hasOwnProperty(key)) continue;
+
+        let item = data[key];
+        let widgetClass = `widget${isWarning ? " warning" : ""}`;
+        let pluginType = "";
+
+        if (typeof item === "object" && item && "Type" in item) {
+            pluginType = item.Type;
+            item = { ...item };
+            delete item.Type;
+        }
+
+        let widgetSize = "";
+        if (levelClass > 1) {
+            widgetSize = "small";
+            if (
+                typeof item === "object" &&
+                item &&
+                !Array.isArray(item) &&
+                Object.keys(item).length > 4
+            ) {
+                widgetSize = "big";
+            }
+            widgetClass += ` ${pluginType}`;
+        }
+        widgetClass += ` ${widgetSize}`;
+
+        if (Object.keys(data).length === 1 && typeof item !== "string") {
+
+            html += renderWidgetData(item, levelClass + 1, renderPluginHeader(Object.keys(data)[0]));
+        } else {
+            html += `<div class="${levelClass} ${widgetClass}">
+                    <div class="widget-title">${key}:</div>
+                    ${renderWidgetData(item, levelClass + 1, template)}
+                 </div>`;
+        }
+    }
+    return Utils.FormatString(template, html);
+}
+
+
+/**
+ * Render plugins data recieved from server
+ */
+export function renderList(data: any, levelClass: number = 0, template: string = "{0}"): string {
+    if (data == null) return "";
+
+    let currentData = { ...data };
+    const isWarning: boolean = currentData.isWarning || false;
+    delete currentData.isWarning;
+
+    let html = "";
+
+    const { html: agentTitleHtml, data: afterAgentData } = renderAgentTitleForWidget(currentData);
+    currentData = afterAgentData;
+
+    html += Utils.FormatString(template, agentTitleHtml);
+
+    let pluginDataHtml = renderPluginData(html, currentData, levelClass + 1, isWarning)
+
+    return "";
+}
+
+function renderWidgetData(data: any, levelClass: number = 0, template: string = "{0}"): string {
+    if (data == null) return "";
+
+    if (Array.isArray(data)) {
+        return renderArray(data);
+    }
+
+    if (typeof data !== "object") {
+        return renderPrimitive(data);
+    }
+
+    let currentData = { ...data };
+    const isWarning: boolean = currentData.isWarning || false;
+    delete currentData.isWarning;
+
+    let pluginDataHtml = renderPluginData(template, currentData, levelClass + 1, isWarning)
+
+    if (pluginDataHtml.length > 0 && levelClass <= 2) {
+        makeNewWidget(`${pluginDataHtml}`);
+        return ""
+    }
+
+    return pluginDataHtml;
+}
+
+
+const toggleSwitch = document.getElementById("toggleSwitch") as HTMLInputElement | null;
+
+/**
+ * Add an event listener to the toggle switch when changed
+ */
+toggleSwitch?.addEventListener("change", hideNonWarningWidgets);
+
+/**
+ * This function hides/shows widgets with warning class
+ */
+export function hideNonWarningWidgets(): void {
+    if (!toggleSwitch) return;
+
+    const widgets = document.querySelectorAll<HTMLElement>(".grid-stack-item-content");
+
+    widgets.forEach((widget) => {
+        if (!widget.classList.contains("parent-warning") && widget.parentElement) {
+            widget.parentElement.style.display = toggleSwitch.checked ? "none" : "";
+        }
+    });
+}
