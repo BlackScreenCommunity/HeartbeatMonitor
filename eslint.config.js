b2bc923c205ed72e545f94aeb47f93ebdbc8b1b9
Nikolai Kuzmin <nickkuzmin1301@gmail.com>
2025-04-27 13:27:35 +0300

    Добавлен eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import eslintParserTs from '@typescript-eslint/parser';
+
+/** @type {import('eslint').Linter.Config} */
+export default [{
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+        parser: eslintParserTs,
+        parserOptions: {
+            project: './tsconfig.json',
+            ecmaVersion: 2020,
+            sourceType: 'module',
+        },
+    },
+    plugins: {
+        '@typescript-eslint': eslintPluginTs,
+    },
+    rules: {
+        ...eslintPluginTs.configs.recommended.rules,
+    },
+}];
