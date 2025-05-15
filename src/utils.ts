0357c38210e012321a0c89728f8c63380f5f9dce
Nikolai Kuzmin <nickkuzmin1301@gmail.com>
2025-05-15 22:06:23 +0300

    Рефакторинг утилитных функций

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,27 @@
 
+/**
+ * Replace {0}, {1} ... {n} in a string with provided values
+ */
 export function FormatString(str: string, ...val: string[]) {
     for (let index = 0; index < val.length; index++) {
-        str = str.replace(`{${index}}`, val[index]);
+        str = str.replace(new RegExp(`\\{${index}\\}`, 'g'), val[index]);
     }
-    
     return str;
 }
 
+/**
+ * Escape string for regexp to prevent parsing errors
+ */
+function escapeRegExp(str: string) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+/**
+ *  Count how many times a pattern appears in a text
+ */
 export function GetOccurencesCount(text: string, search: string) { 
     let trimmedText = text.replace(/ /g, '');
-    let trimmedSearch = search.replace(/ /g, '');
+    let trimmedSearch = escapeRegExp(search.replace(/ /g, ''));
 
     return (trimmedText.match(new RegExp(trimmedSearch, 'g')) || []).length;
 }
